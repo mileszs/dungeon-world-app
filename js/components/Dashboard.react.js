345ee8ef3507a60cc7b4ce0d02c5497ef2aea73f
Miles Z. Sterrett <miles@mileszs.com>
2015-04-11 08:27:47 -0400

    Temporarily remove Roll-related stuff
    It might make a come back, but I don't think it's actually useful for
    the end goal of this app

--- js/components/Dashboard.react.js
+++ js/components/Dashboard.react.js
@@ -2,32 +2,22 @@ import React from 'react';
 import _ from 'lodash';
 
 import CharacterActions from '../actions/CharacterActions';
-import RollActions from '../actions/RollActions';
 import CharacterList from './CharacterList.react';
-import RollHistory from './RollHistory.react';
-import DiceForm from './DiceForm.react';
 import CurrentCharacterDetails from './CurrentCharacterDetails.react';
 
 import CharacterStore from '../stores/CharacterStore';
-import RollStore from '../stores/RollStore';
 
 function getDashboardState() {
   let current = CharacterStore.current()
-  let rolls = []
-  if (current) {
-    rolls = RollStore.getCharacterRolls(current.id);
-  }
   return {
     characters: CharacterStore.getAll(),
     current: current,
-    rolls: rolls,
   };
 }
 
 let Dashboard = React.createClass({
   getInitialState() {
     CharacterActions.load()
-    RollActions.load()
     return getDashboardState()
   },
 
@@ -42,20 +32,16 @@ let Dashboard = React.createClass({
         </div>
         <div className="row">
           <CurrentCharacterDetails current={this.state.current} />
-          <RollHistory rolls={this.state.rolls} />
-          <DiceForm currentCharacter={this.state.current}/>
         </div>
       </div>
     )
   },
 
   componentDidMount() {
-    RollStore.addChangeListener(this._onChange);
     CharacterStore.addChangeListener(this._onChange);
   },
 
   componentWillUnmount() {
-    RollStore.removeChangeListener(this._onChange);
     CharacterStore.removeChangeListener(this._onChange);
   },
 
